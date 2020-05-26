70d3ef1c7ed586dcfabc2b66104958892c5c8be7
Pharaoh <pharaohmanson@gmail.com>
2020-05-25 22:40:29 -0400

    Initial commit

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key] //key will automatically add Id as an Identity column, so we wont have to pass the value
+        public int Id { get; set; }
+
+        [Required] //means this property cannot be null
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+    }
+}
---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db; //dependency injection.. we need db objects to display on this page
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+                
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGetAsync() //this method is going into the db getting all books and storing it inside IEnum books
+        {
+            Books = await _db.Book.ToListAsync(); //async lets you run multiple tasks at a time until its awaiting.. we need to await because we need to wait until we assing all the books that we found
+
+        }
+    }
+}
